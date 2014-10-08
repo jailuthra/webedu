dfa5c369bcd9230e79af84031164cd13329f92d2
Jai Luthra <me@jailuthra.in>
2014-10-08 14:20:20 +0530

    player: Add previous and next buttons

--- app.py
+++ app.py
@@ -6,25 +6,12 @@
 from flask import Flask, url_for, render_template
 from glob import glob
 import os
-from courses import course_map, update_lectures
+import courses
 
 app = Flask(__name__)
 
-course_lectures = dict()
-
-@app.route('/course/<course_id>/<int:lec_no>')
-def player(course_id, lec_no):
-    return render_template('player.html', course_name = course_map[course_id],
-                            lec_no = lec_no, lec_path = course_lectures[course_id][lec_no])
-
-@app.route('/course/<course_id>')
-def course(course_id=None):
-    global course_lectures
-    course_lectures = update_lectures()
-    lectures = course_lectures[course_id]
-    return render_template('course.html', course_id = course_id,
-                            course_name = course_map[course_id],
-                            lectures = lectures)
+course_lectures = courses.update_lectures()
+course_map = courses.course_map
 
 @app.route('/')
 def home():
@@ -33,6 +20,24 @@ def home():
         course_id = os.path.basename(course_path)
         courses.append((course_id, course_map[course_id]))
     return render_template('index.html', courses = courses)
+
+@app.route('/course/<course_id>')
+def course(course_id=None):
+    lectures = course_lectures[course_id]
+    return render_template('course.html', course_id = course_id,
+                            course_name = course_map[course_id],
+                            lectures = lectures)
+
+@app.route('/course/<course_id>/<int:lec_no>')
+def player(course_id, lec_no):
+    args = {
+        'course_name': course_map[course_id],
+        'course_id': course_id,
+        'lec_no': lec_no,
+        'lec_path': course_lectures[course_id][lec_no],
+        'total_lectures': len(course_lectures[course_id]),
+    }
+    return render_template('player.html', args = args)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
